a62357be6e35ccee29cd9f6e736c428e619b3574
abdullahzahid321456 <abdullahzahid321456@gmail.com>
2022-06-03 16:11:45 +0500

    stripe integration

--- src/backend/Endpoints/Stripe/AddCard.js
+++ src/backend/Endpoints/Stripe/AddCard.js
@@ -5,11 +5,14 @@ import {
     useElements
 } from "@stripe/react-stripe-js";
 import {localUrl} from "../../../utils/constants";
+import Grid from "@material-ui/core/Grid/Grid";
+import Button from "@material-ui/core/Button/Button";
+
 export default function AddCard(props) {
     const stripe = useStripe();
     const elements = useElements();
 
-    const [message,setMessage]=useState("");
+    const [message, setMessage] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const [isCardAdded, setIsCardAdded] = useState({msg: "", status: false});
 
@@ -27,15 +30,15 @@ export default function AddCard(props) {
         }
 
         stripe.retrieveSetupIntent(clientSecret).then(({setupIntent}) => {
-            console.log("setupIntent111 = ",setupIntent)
-            if(props.setSetupIntent) props.setSetupIntent(setupIntent.payment_method)
+            console.log("setupIntent111 = ", setupIntent)
+            if (props.setSetupIntent) props.setSetupIntent(setupIntent.payment_method)
             switch (setupIntent.status) {
                 case "succeeded":
                     setIsCardAdded({status: true, msg: "Success! Your payment method has been saved!"});
-                    if(props.setIsPricing) props.setIsPricing(true)
+                    if (props.setIsPricing) props.setIsPricing(true)
                     props.createSubscription(setupIntent.payment_method);
                     setIsLoading(false)
-                    localStorage.setItem("status",setupIntent.status)
+                    localStorage.setItem("status", setupIntent.status)
                     break;
                 case "processing":
                     setIsCardAdded({
@@ -60,10 +63,10 @@ export default function AddCard(props) {
     }, [stripe]);
 
 
-    useEffect(()=>{
-        if(isCardAdded.status){
+    useEffect(() => {
+        if (isCardAdded.status) {
         }
-    },[isCardAdded])
+    }, [isCardAdded])
 
     const handleSubmit = async (e) => {
 
@@ -107,23 +110,33 @@ export default function AddCard(props) {
 
     };
 
-    console.log("message = ", !stripe,"   elements = ",!elements,isLoading)
+    console.log("message = ", !stripe, "   elements = ", !elements, isLoading)
     return (
         <Fragment>
-
-            {!isCardAdded.status && isLoading ?
-                <div id="payment-form" onClick={handleSubmit}>
-                    <PaymentElement id="payment-element"/>
-                    <button disabled={ !stripe || !elements} id="submit">
+            <Grid container>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={10} style={{width:"100%"}} container>
+                    {!isCardAdded.status && isLoading ?
+                        <Fragment>
+                            <Grid container xs={12} id="payment-form" onClick={handleSubmit} justifyContent="center" alignItems="center">
+                                <div style={{width:"100%"}}>
+                                    <PaymentElement id="payment-element" />
+                                </div>
+                            </Grid>
+                            <Grid container xs={12} style={{marginTop:10}}>
+                                <Button disabled={!stripe || !elements} id="submit" onClick={handleSubmit}>
                             <span id="button-text">
                             Add Card
                             </span>
-                    </button>
-                    {/* Show any error or success messages */}
-                    {message && <div id="payment-message">{message}</div>}
-                </div>
-                :
-                <p style={{color:isCardAdded.status?"#20c997":'red'}} >{isCardAdded.msg}</p> }
+                                </Button>
+                            </Grid>
+                        </Fragment>
+                        :
+                        <p style={{color: isCardAdded.status ? "#20c997" : 'red'}}>{isCardAdded.msg}</p>}
+                </Grid>
+                <Grid item xs={1}></Grid>
+            </Grid>
+
         </Fragment>
     );
 }
